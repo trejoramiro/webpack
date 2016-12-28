6b2be1ee57c42702cf506b695ebb671a06403581
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-27 20:48:12 -0600

    Example of inline loader for Jade

--- webpack_configs_0104/client/js/app.js
+++ webpack_configs_0104/client/js/app.js
@@ -11,3 +11,7 @@ console.log(person);
 const { name, age } = person
 
 console.log(`${name} is ${age} old!`);
+
+const view = require("raw!jade-html?pretty=true!../views/index.jade");
+console.log(view)
+document.body.innerHTML = view;
